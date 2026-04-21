d1245297368d7c1a169d2c0db339d4cf6ab8e956
Deepak7200 <mail.deepaksehrawat@gmail.com>
2026-04-21 18:47:03 +0530

    image added

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ export const razorpay = new Razorpay({
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, async () => {
+app.listen( PORT, async () => {
     await connectionToDB();
     console.log(`App is running on port ${process.env.PORT}`);
 });
